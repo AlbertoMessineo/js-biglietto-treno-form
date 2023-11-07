cbb7a144717ab4c91f656d2dd8ca93c2f80d0a0e
AlbertoMessineo <albevorox@gmail.com>
2023-11-07 22:45:21 +0100

    feat: scaffolding

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 ## MILESTONE 1
 
 -Attraverso due tag <input> prendo informazioni dall'utente (Nome, distanza in km)
+
 -Con un tag <select> creo la tendina da cui l'utente sceglie la fascia d'età
+
 -Con getElementById nel file Javascript accedo ai dati inseriti dall'utente
---
+++ myscript.js
@@ -0,0 +1,27 @@
+'use strict';
+
+//PROMPT DATA
+// const age = prompt('inserire la sua età');
+// const distance = prompt('inserire la distanza da percorrere (in km)');
+
+// TRASFORMO IN NUMERO LE STRINGHE (DATI DEI PROMPT)
+console.log('età inserita ' + +age);
+console.log('distanza da percorrere in km ' + +distance);
+
+// CALCOLO IL PREZZO DEL BIGLIETTO
+const ticket= distance*0.21;
+
+// CONDIZIONI DEGLI SCONTI EVENTUALI
+
+// sconto 20% per minorenni
+if(+age < '18'){
+    console.log('il prezzo del biglietto per minorenni è ' + ticket *0.8 +' €');
+}
+// sconto 40% per over 65
+else if(+age >= '65'){
+    console.log('il prezzo del biglietto per gli over 65 è ' + ticket *0.6 +' €');
+}
+// condizione nessuno sconto
+else{
+    console.log('il prezzo del biglietto è ' + ticket.toFixed(2) + ' €');
+}
