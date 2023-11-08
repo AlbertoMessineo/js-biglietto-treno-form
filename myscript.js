cdb1205afda1fc1c66b96731c77eea8b97f7b0f4
AlbertoMessineo <albevorox@gmail.com>
2023-11-08 11:59:50 +0100

    feat: es completed

--- myscript.js
+++ myscript.js
@@ -1,51 +1,88 @@
 'use strict';
 
 //INPUT DATA
+const elementName = document.getElementById('name-id');
+const elementKmUtente = document.getElementById('distance-id');
+const elementAge = document.querySelector('#age-selector');
+const elementButtonGenerator = document.getElementById('generate');
+const elementClear = document.getElementById('clean');
+const elementTicket = document.getElementById('biglietto-virtuale')
+
+// dati costo biglietto
+const costoBigliettoKm = 0.21;
 
 
-// const bottone=document.getElementById("generate")
-//  bottone.addEventListener("click", function(){
-
-//     let distance = document.getElementById("distance-id").value
-// distance = parseInt(distance)
-// console.log("Numero di km:", NumeroKm)
-
-
-
-
-
-
-
-
-
-
-
-// const distance = document.getElementById('distance-id').value;
-const age = document.getElementById('age-selector').value;
+// campi biglietto virtuale
+const elementUserName = document.getElementById('nome-utente');
+const elementTariff = document.getElementById('tariffa');
+const elementCarrozza = document.getElementById('n-carrozza');
+const elementCpCode = document.getElementById('cp-code');
+const elementFinalPrice = document.getElementById('final-price');
 
 
 
 
-// const distance = prompt('inserir;e la distanza da percorrere (in km)');
+// calcolo del biglietto
+elementButtonGenerator.addEventListener('click', function(){
 
-// TRASFORMO IN NUMERO LE STRINGHE (DATI DEI PROMPT)
-console.log('età inserita ' + +age);
-console.log('distanza da percorrere in km ' + +distance);
+   let ticketPrice= costoBigliettoKm * Number(elementKmUtente.value);
+    
+    console.log(ticketPrice);
 
-// CALCOLO IL PREZZO DEL BIGLIETTO
-const ticket= distance*0.21;
+    if(elementAge.value === 'minorenne'){
 
-// CONDIZIONI DEGLI SCONTI EVENTUALI
+     // applico sconto 20%
+    ticketPrice -= (ticketPrice * 20) / 100;
 
-// sconto 20% per minorenni
-if(+age < '18'){
-    console.log('il prezzo del biglietto per minorenni è ' + ticket *0.8 +' €');
-}
-// sconto 40% per over 65
-else if(+age >= '65'){
-    console.log('il prezzo del biglietto per gli over 65 è ' + ticket *0.6 +' €');
-}
-// condizione nessuno sconto
-else{
-    console.log('il prezzo del biglietto è ' + ticket.toFixed(2) + ' €');
-}
+
+
+    }else if(elementAge.value === 'over-65'){
+
+         // applico sconto 40%
+        ticketPrice -= (ticketPrice * 40) / 100;
+
+    }
+    console.log('Il prezzo del biglietto è ' + ticketPrice + ' euro');
+
+   
+    // faccio comparire il biglietto
+    elementTicket.classList.remove('hidden');
+
+
+     // assegno valori dom
+    elementUserName.innerHTML= elementName.value;
+    // elementTariff.innerHTML= elementName.value;
+    elementFinalPrice.innerHTML= ticketPrice.toFixed(2) + ` €`;
+    elementCarrozza.innerHTML =  Math.floor(Math.random() * 19 + 1);
+    elementCpCode.innerHTML =  Math.floor(Math.random()*90000) + 10000;
+
+
+
+
+
+
+ 
+
+
+
+});
+
+
+
+// reset dei dati 
+elementClear.addEventListener('click', function(){
+    console.log('Svuoto i campi di input e nascondo il biglietto virtuale');
+
+    elementKmUtente.value='';
+    elementName.value='';
+
+
+
+
+
+
+    elementTicket.classList.add('hidden');
+    
+
+});
+
